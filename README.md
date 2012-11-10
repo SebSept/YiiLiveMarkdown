5ea73b543313caf0d33616577d431c6d555c990c
SebSept <sebastienmonterisi@yahoo.fr>
2012-11-11 00:19:23 +0100

    moved style to css file, larger div for textarea and render div #8 #4.

--- README.md
+++ README.md
@@ -13,15 +13,26 @@ You may need to change path to framework in `/index.php` by editing `$yii`.
 - Change the ajax refresh interval time in `protected/config/main.php`, edit `md_refreshInterval` value. 
 ( Default to 1000 miliseconds. )
 
-## How 
+## How is it made
 
-- It use [yii build in markdown class](http://www.yiiframework.com/doc/api/1.1/CMarkdown).
-- Trigger an ajax request each second (default) to convert markdown to html (if user type only)
+Trigger an ajax request each second (default) to convert markdown to html (if user types only)
+
+Uses
+
+- [yii build in markdown class](http://www.yiiframework.com/doc/api/1.1/CMarkdown).
+- [One% CSS Grid](http://onepcssgrid.mattimling.com/) 
 
 ## Why 
 
-I just made it to write README.md files (for github). (Yes, i know other solutions exists but you can consider it as simple yii tiny app example).
+I just made it to 
+
+- write README.md files (for github). (Yes, i know other solutions exists but you can consider it as simple yii tiny app example).
+- train with git and github.
+
+## Licence
+
+[There should be one](https://github.com/SebSept/YiiLiveMarkdown/issues/10) ... (feel free to give advice). 
 
 ## Contribute
 
-Feel free to contribute and make pull request. Read TODO.md for ideas.
+[Feel free to contribute](https://github.com/SebSept/YiiLiveMarkdown/issues) and make pull request. 
--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -5,31 +5,24 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta name="language" content="en" />
 
-	<!-- blueprint CSS framework -->
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/screen.css" media="screen, projection" />
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/print.css" media="print" />
-	<!--[if lt IE 8]>
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/ie.css" media="screen, projection" />
-	<![endif]-->
-
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css" />
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" />
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/onepcssgrid.css" media="screen, projection" />
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css" media="screen, projection" />
 
 	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
 </head>
 
 <body>
 
-<div class="container" id="page">
+<div class="onepcssgrid-1200" id="page">
 
-	<div id="header">
+	<div id="header" >
 		<div id="logo"><?php echo CHtml::encode(Yii::app()->name); ?></div>
 	</div><!-- header -->
 
 	<div id="mainmenu">
 		<?php $this->widget('zii.widgets.CMenu',array(
 			'items'=>array(
-				array('label'=>'Home', 'url'=>array('/site/index')),
+				array('label'=>'Converter', 'url'=>array('/site/index')),
 				array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
 			),
 		)); ?>
@@ -37,9 +30,9 @@
 
 	<?php echo $content; ?>
 
-	<div class="clear"></div>
+<!--	<div class="clear"></div> -->
 
-	<div id="footer">
+	<div id="footer" class="onerow">
 		Copyright &copy; <?php echo date('Y'); ?> by My Company.<br/>
 		All Rights Reserved.<br/>
 		<?php echo Yii::powered(); ?>
