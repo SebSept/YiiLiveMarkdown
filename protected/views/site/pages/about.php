6ed3b21ccb68205945697b05c4fc3b4d424fec9d
SebSept <sebastienmonterisi@yahoo.fr>
2012-11-11 13:55:59 +0100

    content on about page

--- protected/views/site/pages/about.php
+++ protected/views/site/pages/about.php
@@ -6,7 +6,12 @@ $this->breadcrumbs=array(
 	'About',
 );
 ?>
-<h1>About</h1>
+<h1>About <?php echo yii::app()->name; ?></h1>
 
-<p>This is a "static" page. You may change the content of this page
-by updating the file <code><?php echo __FILE__; ?></code>.</p>
+<p>It's a tiny webapp build with <a href="http://www.yiiframework.com">the Yii Framework</a>.</p>
+
+<p>Source code is on github <a href="https://github.com/SebSept/YiiLiveMarkdown">SebSept/YiiLiveMarkdown</a>.</p>
+
+<p>It was made to easyly write markdown file for github (README.md).</p>
+
+<p>Sure other apps exists (one in ruby made by github). I made it to train with github in realcode situation.</p>
